858b3b2a11edc82d78cb9eaae3918e4dbbbdaadc
Sylvie Howton <sylviehowton@sylvies-air-665.home>
2020-04-15 11:16:18 -0700

    nav link selected state

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
   $('#nav-placeholder').load('navigation.html');
   $('#footer-placeholder').load('footer.html');
+
+  $(function() {
+  $('.nav_li a[href^="/' + location.pathname.split("/")[1] + '"]').addClass('active');
+  console.log('test');
+});
+
 });
